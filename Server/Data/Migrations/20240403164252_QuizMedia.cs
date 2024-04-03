44697002bea59b2a6ce3c379fff28d977d527d91
Ilyas Kaya <113895247+AkiVonAkira@users.noreply.github.com>
2024-04-03 18:49:47 +0200

    quiz media migration

---
+++ Server/Data/Migrations/20240403164252_QuizMedia.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizApp.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuizMedia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuizMediaId",
+                table: "Quizzes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quizzes_QuizMediaId",
+                table: "Quizzes",
+                column: "QuizMediaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quizzes_Media_QuizMediaId",
+                table: "Quizzes",
+                column: "QuizMediaId",
+                principalTable: "Media",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quizzes_Media_QuizMediaId",
+                table: "Quizzes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Quizzes_QuizMediaId",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "QuizMediaId",
+                table: "Quizzes");
+        }
+    }
+}
